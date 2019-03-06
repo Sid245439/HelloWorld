a02b22dda88a6ea72c619391c6685cb12cb1dc65
Sid245439 <25339664+Sid245439@users.noreply.github.com>
2019-03-06 11:16:11 +0530

    Update web.js

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var http = require('http');
 //loads http module
 var app=http.createServer(function (req, res) {
 //creates server
+  console.log("Inside Server")
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
   res.end('Hello World\n');
